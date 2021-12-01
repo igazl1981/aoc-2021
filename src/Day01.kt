f4cc408212dd8004bc506e671a11c44d0113ab58
laszlo-igaz-ah <laszlo.igaz.epam+ah@gmail.com>
2021-12-01 18:33:11 +0100

    Added AOC 2021 Part2 solution * Used Int mapping for input to simplify solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Int>): Int {
         
         return input
-            .map { it.toInt() }
-            .filterIndexed { index, measurement -> index > 0 && measurement > input[index - 1].toInt() }
+            .filterIndexed { index, measurement -> index > 0 && measurement > input[index-1] }
             .count()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val windows = input
+            .take(input.size - 2)
+            .mapIndexed { index, current -> current + input[index + 1] + input[index + 2] }
+
+        return part1(windows)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    println(part1(testInput))
-    check(part1(testInput) == 7)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    val part1Result = part1(testInput)
+    println(part1Result)
+    check(part1Result == 7) { "Part1 failed: $part1Result" }
+    val part2Result = part2(testInput)
+    check(part2Result == 5) { "Part2 failed: $part2Result" }
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println("Part1: ${part1(input)}")
     println("Part2: ${part2(input)}")
 }
