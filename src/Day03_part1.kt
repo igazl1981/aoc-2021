a8bd30a649bbafc356853a3bd0d0c4fcedc1c86c
laszlo-igaz-ah <laszlo.igaz.epam+ah@gmail.com>
2021-12-04 17:28:05 +0100

    Added AOC 2021 Day3 part 2 solution

--- src/Day03_part1.kt
+++ src/Day03_part1.kt
@@ -1,3 +1,4 @@
+
 fun main() {
     fun part1(input: List<String>): Int {
         val gamma = mutableListOf<Int>()
@@ -25,29 +26,12 @@ fun main() {
         return gammaDecimal * epsilonDecimal
     }
 
-    fun part2(input: List<String>): Int {
-        return 1
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     val part1Result = part1(testInput)
     println(part1Result)
     check(part1Result == 198) { "Part1 failed: $part1Result" }
-    val part2Result = part2(testInput)
-    println(part2Result)
-    check(part2Result == 1) { "Part2 failed: $part2Result" }
 
-    val input = readInput("Day03")
-    println("Part1: ${part1(input)}")
-    check(part1(input) == 4191876) { "Part1 failed: ${part1(input)}" }
-    println("Part2: ${part2(input)}")
-}
-
-enum class Direction1 {
-    DOWN, FORWARD, UP;
-
-    companion object {
-        fun fromString(source: String) = valueOf(source.uppercase())
-    }
+    val inputPart1 = readInput("Day03")
+    println("Part1: ${part1(inputPart1)}")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -17,3 +17,25 @@ fun readInputForDay2(name: String) = readInput(name)
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Returns the size of the list and 0 if the list is null
+ */
+fun getSizeOfItems(items: List<String>?) = items?.size ?: 0
+
+
+fun findRating(input: List<String>, conditionToRetriveZeros: (groupedByBit: Map<Char, List<String>>) -> Boolean): String {
+    val binaryLength = input.first().length
+    var filteredInput = input
+    (0 until binaryLength).asSequence().takeWhile { filteredInput.size > 1 }
+        .forEach { i ->
+            val grouped = filteredInput.groupBy { it[i] }
+            if (conditionToRetriveZeros(grouped)) {
+                filteredInput = grouped['0']!!
+            } else {
+                filteredInput = grouped['1']!!
+            }
+        }
+
+    return filteredInput.first()
+}
